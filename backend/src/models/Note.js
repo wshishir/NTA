3b0750c3038bdcf61243668dfb536f41b00b1457
Shishir <shishirtech506@gmail.com>
2025-08-05 23:30:43 +0530

    model done

---
+++ backend/src/models/Note.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+
+const noteSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }  //createdAt,updatedAt
+)
+
+const Note = mongoose.model("Note", noteSchema);
+
+export default Note;
--- backend/src/server.js
+++ backend/src/server.js
@@ -8,9 +8,10 @@ dotenv.config();
 connectDB();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(8800, () => {
-  console.log("Server is running on port: 8800");
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
 });
