8ddc2b1fc1b5b1a460f7e31a1ba8672276c54306
Shishir <shishirtech506@gmail.com>
2025-08-20 04:53:56 +0530

    build errors

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
-import { connectDB } from "./config/database.js";
+import  connectDB from "./config/database.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
